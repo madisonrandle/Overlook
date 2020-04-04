44d73568b0fed82ccec818fb59aa04b9209b150a
Madison Randle <madisonrandle25@gmail.com>
2020-04-04 17:16:37 -0600

    Write tests to verify hotel is a function and instance

--- test/Hotel-test.js
+++ test/Hotel-test.js
@@ -1,9 +1,21 @@
 import chai from 'chai';
 import Hotel from '../src/Hotel'
-import bookingsData from '../src/test-data/bookings-test-data';
+import usersData from '../src/test-data/users-test-data';
 import roomsData from '../src/test-data/rooms-test-data';
+import bookingsData from '../src/test-data/bookings-test-data';
 const expect = chai.expect;
 
+let hotel;
 describe('Hotel', () => {
+  beforeEach(() => {
+    hotel = new Hotel(usersData, roomsData, bookingsData);
+   });
 
+  it('should be a function', function() {
+       expect(Hotel).to.be.a('function');
+   });
+
+  it('should be an instance of Hotel', function() {
+   expect(hotel).to.be.an.instanceof(Hotel);
+  });
 });
