cc60eeb1611a4c1f3673d91e55eedad8358bd7c1
Madison Randle <madisonrandle25@gmail.com>
2020-04-04 17:43:33 -0600

    Write tests for availableRooms and occupiedRooms property

--- test/Manager-test.js
+++ test/Manager-test.js
@@ -6,16 +6,24 @@ import roomsData from '../src/test-data/rooms-test-data';
 const expect = chai.expect;
 
 let manager;
-describe('Manager', () => {
+describe.only('Manager', () => {
   beforeEach(() => {
     manager = new Manager();
    });
 
-  it('should be a function', function() {
+  it('should be a function', () => {
        expect(Manager).to.be.a('function');
    });
 
-  it('should be an instance of Hotel', function() {
+  it('should be an instance of Hotel', () => {
    expect(manager).to.be.an.instanceof(Manager);
   });
+
+  it('should accept the total rooms available', () => {
+    expect(manager.availableRooms).to.be.a('array');
+  });
+
+  it('should accept the total rooms occupied', () => {
+    expect(manager.occupiedRooms).to.be.a('array');
+  })
 });
