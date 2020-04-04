f32ef85cedbd2cb256112f93c5e1ebc047c99b3c
Madison Randle <madisonrandle25@gmail.com>
2020-04-04 17:42:57 -0600

    Add availableRooms and occupiedRooms properties

--- src/Manager.js
+++ src/Manager.js
@@ -1,9 +1,14 @@
-import $ from 'jquery'
+import $ from 'jquery';
+import Hotel from './Hotel';
 
-class Manager {
-  constructor() {
-
+class Manager extends Hotel {
+  constructor(usersData, roomsData, bookingsData) {
+    super(usersData, roomsData, bookingsData)
+      this.availableRooms = [];
+      this.occupiedRooms = [];
   }
+
+  
 }
 
 export default Manager;
