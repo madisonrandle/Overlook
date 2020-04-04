c2f8eaad04d31ad732cceefc062d81e02c2ac50f
Madison Randle <madisonrandle25@gmail.com>
2020-04-04 17:16:49 -0600

    Write tests to verify manager is a function and instance

--- test/Manager-test.js
+++ test/Manager-test.js
@@ -1,12 +1,21 @@
 import chai from 'chai';
 import Manager from '../src/Manager';
-import Customer from '../src/Customer';
-import Hotel from '../src/Hotel';
 import usersData from '../src/test-data/users-test-data';
 import bookingsData from '../src/test-data/bookings-test-data';
 import roomsData from '../src/test-data/rooms-test-data';
 const expect = chai.expect;
 
+let manager;
 describe('Manager', () => {
+  beforeEach(() => {
+    manager = new Manager();
+   });
 
+  it('should be a function', function() {
+       expect(Manager).to.be.a('function');
+   });
+
+  it('should be an instance of Hotel', function() {
+   expect(manager).to.be.an.instanceof(Manager);
+  });
 });
