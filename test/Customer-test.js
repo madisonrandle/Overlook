12d523650e9495fb9db3233fecd1efc4065174ab
Madison Randle <madisonrandle25@gmail.com>
2020-04-04 17:16:23 -0600

    Write tests to verify customer is a function and instance

--- test/Customer-test.js
+++ test/Customer-test.js
@@ -1,11 +1,21 @@
 import chai from 'chai';
 import Customer from '../src/Customer';
-import Hotel from '../src/Hotel';
 import usersData from '../src/test-data/users-test-data';
 import bookingsData from '../src/test-data/bookings-test-data';
 import roomsData from '../src/test-data/rooms-test-data';
 const expect = chai.expect;
 
+let customer;
 describe('Customer', () => {
+  beforeEach(() => {
+    customer = new Customer();
+   });
 
+  it('should be a function', function() {
+       expect(Customer).to.be.a('function');
+   });
+
+  it('should be an instance of Hotel', function() {
+   expect(customer).to.be.an.instanceof(Customer);
+  });
 });
