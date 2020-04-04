f59b7c28a7b456e5d2a5a79159c83725d74a7c2c
Madison Randle <madisonrandle25@gmail.com>
2020-04-04 17:17:56 -0600

    Remove function containing Promise.all method

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
 import $ from 'jquery';
 import './css/main.scss';
 import moment from 'moment';
+import Hotel from './Hotel';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
 
 const usersData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
   .then(response => response.json())
-  .then(data => console.log(data.users))
+  .then(data => data.users)
   .catch(error => console.log(`There was an error: ${error}`));
 
 const roomsData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
   .then(response => response.json())
-  .then(data => console.log(data.rooms))
+  .then(data => data.rooms)
   .catch(error => console.log(`There was an error: ${error}`));
 
 const bookingsData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
   .then(response => response.json())
-  .then(data => console.log(data.bookings))
+  .then(data => data.bookings)
   .catch(error => console.log(`There was an error: ${error}`));
+
 
 Promise.all([usersData, roomsData, bookingsData])
   .then(data => {
-    const usersData = data[0];
-    const roomsData = data[1];
-    const bookingsData = data[2];
+    let hotel = new Hotel(data[0], data[1], data[2]);
   })
   .catch(error => console.log(`There was an error: ${error}`));
+
+
 
 
 
