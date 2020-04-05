876042a6ce4f3debc764b9f90fa92d5b159dacc0
Madison Randle <madisonrandle25@gmail.com>
2020-04-04 18:16:09 -0600

    Remove moment import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import $ from 'jquery';
 import './css/main.scss';
-import moment from 'moment';
 import domUpdates from './domUpdates';
 import Hotel from './Hotel';
 
@@ -25,6 +24,6 @@ const bookingsData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/b
 Promise.all([usersData, roomsData, bookingsData])
   .then(data => {
     let hotel = new Hotel(data[0], data[1], data[2]);
-    domUpdates.loadPage(hotel);
+    domUpdates.loadLoginPage(hotel);
   })
   .catch(error => console.log(`There was an error: ${error}`));
