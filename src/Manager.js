b7904a79e7bc556fbd48801250f75ac24df8855b
Madison Randle <madisonrandle25@gmail.com>
2020-04-04 18:15:15 -0600

    Extend todaysDate property from Hotel class

--- src/Manager.js
+++ src/Manager.js
@@ -1,14 +1,15 @@
 import $ from 'jquery';
 import Hotel from './Hotel';
 
+
 class Manager extends Hotel {
-  constructor(usersData, roomsData, bookingsData) {
-    super(usersData, roomsData, bookingsData)
+  constructor(usersData, roomsData, bookingsData, todaysDate) {
+    super(usersData, roomsData, bookingsData, todaysDate)
       this.availableRooms = [];
       this.occupiedRooms = [];
   }
 
-  
+
 }
 
 export default Manager;
