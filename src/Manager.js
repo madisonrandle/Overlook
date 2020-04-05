374456efd94a33e4251022535024b7cf2c19c4e3
Madison Randle <madisonrandle25@gmail.com>
2020-04-04 20:52:32 -0600

    Write method that returns the total revenue for a given day

--- src/Manager.js
+++ src/Manager.js
@@ -1,32 +1,46 @@
 import $ from 'jquery';
 import Hotel from './Hotel';
 
-
 class Manager extends Hotel {
-  constructor(usersData, roomsData, bookingsData, todaysDate) {
-    super(usersData, roomsData, bookingsData, todaysDate)
+  constructor(users, rooms, bookings, todaysDate) {
+    super(users, rooms, bookings, todaysDate)
       this.availableRooms = [];
       this.occupiedRooms = [];
   };
 
   getAvailableRoomsToday() {
     let roomsBookedToday = [];
-    this.roomsData.forEach(room => {
-      this.bookingsData.forEach(booking => {
+    this.rooms.forEach(room => {
+      this.bookings.forEach(booking => {
         if (booking.roomNumber === room.number && booking.date === this.todaysDate) {
           roomsBookedToday.push(room);
         };
       });
     });
 
-    this.roomsData.forEach(room => {
-      this.bookingsData.forEach(booking => {
+    this.rooms.forEach(room => {
+      this.bookings.forEach(booking => {
         if (!roomsBookedToday.includes(room) && !this.availableRooms.includes(room)) {
           this.availableRooms.push(room);
         }
       })
     });
     return this.availableRooms;
+  };
+
+  getTotalRevenueToday() {
+    return this.rooms.reduce((totalRevenue, room) => {
+      this.bookings.forEach(booking => {
+        if (booking.date === '2020/01/11' && room.number === booking.roomNumber) {
+          totalRevenue += room.costPerNight;
+        };
+      });
+      return totalRevenue;
+    }, 0);
+  };
+
+  getPercentageOfRoomsOccupiedToday() {
+    
   };
 
 };
