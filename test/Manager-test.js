be1e5167cefc4758d89f14b32b8a89a167171225
Madison Randle <madisonrandle25@gmail.com>
2020-04-04 18:56:41 -0600

    Update max lin length to 100

--- test/Manager-test.js
+++ test/Manager-test.js
@@ -1,14 +1,15 @@
 import chai from 'chai';
+import moment from 'moment';
 import Manager from '../src/Manager';
 import usersData from '../src/test-data/users-test-data';
-import bookingsData from '../src/test-data/bookings-test-data';
 import roomsData from '../src/test-data/rooms-test-data';
+import bookingsData from '../src/test-data/bookings-test-data';
 const expect = chai.expect;
 
 let manager;
 describe.only('Manager', () => {
   beforeEach(() => {
-    manager = new Manager();
+    manager = new Manager(usersData, roomsData, bookingsData);
    });
 
   it('should be a function', () => {
@@ -19,11 +20,17 @@ describe.only('Manager', () => {
    expect(manager).to.be.an.instanceof(Manager);
   });
 
-  it('should accept the total rooms available', () => {
+  it('should know what today\'s date is', () => {
+    expect(manager.todaysDate).to.equal(moment().format('L'));
+  });
+
+  it('should accept an array of all available rooms', () => {
     expect(manager.availableRooms).to.be.a('array');
   });
 
-  it('should accept the total rooms occupied', () => {
+  it('should accept an array of all occupied rooms', () => {
     expect(manager.occupiedRooms).to.be.a('array');
-  })
+  });
+
+
 });
