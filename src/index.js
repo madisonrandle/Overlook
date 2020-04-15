7fba8829702667bc987ee99539764d37aeb18024
Madison Randle <madisonrandle25@gmail.com>
2020-04-15 17:11:20 -0600

    Create fetch method to retrieve all data from ApiHandler class

--- src/index.js
+++ src/index.js
@@ -2,22 +2,20 @@
 // import './css/main.scss';
 import domUpdates from './domUpdates';
 import Hotel from './Hotel';
-import Api from './Api';
+import ApiHandler from './ApiHandler';
 
-const apiHandler = new ApiHandler();
+const fetchAllData = () => {
+  const apiHandler = new ApiHandler();
+  const usersData = apiHandler.fetchUsersData();
+  const roomsData = apiHandler.fetchRoomsData();
+  const bookingsData = apiHandler.fetchBookingsData();
 
-
-//seperate function
-// would essentially run when validation for login is confirmed
-// eg => if pass && username .val()
-
-let usersData = api.fetchUsersData();
-let roomsData = api.fetchRoomsData();
-let bookingsData = api.fetchBookingsData();
-Promise.all([usersData, roomsData, bookingsData])
+  Promise.all([usersData, roomsData, bookingsData])
   .then(data => {
-    let hotel = new Hotel(data[0], data[1], data[2]);
-    // console.log('hotel in promise: ', hotel);
+    const hotel = new Hotel(data[0], data[1], data[2]);
     domUpdates.loadLoginPage(hotel);
   })
   .catch(error => console.log(`There was an error: ${error}`));
+};
+
+fetchAllData();
