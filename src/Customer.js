324887f95a6f3dfb12c097ff25c121d73f667681
Madison Randle <madisonrandle25@gmail.com>
2020-04-16 07:56:15 -0600

    Write method to find all bookings for a user

--- src/Customer.js
+++ src/Customer.js
@@ -6,5 +6,12 @@ class Customer {
     this.name = user.name;
     this.isManager = isManager;
   }
+
+  getAllBookings(bookings) {
+    return bookings.reduce((acc, booking) => {
+      booking.userID === this.id && acc.push(booking);
+      return acc;
+    }, []);
+  }
 }
 export default Customer;
