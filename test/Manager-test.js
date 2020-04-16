86ff3249ca32d10e55fdf1ab467a8f424cd910e7
Madison Randle <madisonrandle25@gmail.com>
2020-04-16 01:36:53 -0600

    Begin refactoring Manager test to check number of available rooms

--- test/Manager-test.js
+++ test/Manager-test.js
@@ -21,29 +21,23 @@ describe('Manager', () => {
    expect(manager).to.be.an.instanceof(Manager);
   });
 
-  // it('should know what today\'s date is', () => {
-  //   expect(manager.todaysDate).to.equal(moment().format('YYYY-MM-DD'));
-  // });
-  //
-  // it('should accept an array of all available rooms', () => {
-  //   expect(manager.availableRooms).to.be.a('array');
-  // });
-  //
-  // it('should accept an array of all occupied rooms', () => {
-  //   expect(manager.occupiedRooms).to.be.a('array');
-  // });
-  //
-  // it('should return an array of available rooms for today\'s date', () => {
-  //   expect(manager.getAvailableRoomsToday().length).to.equal(12);
-  // });
-  //
+  it('should accept an array of all available rooms', () => {
+    expect(manager.availableRooms).to.be.a('array');
+  });
+
+  it('should accept an array of all occupied rooms', () => {
+    expect(manager.occupiedRooms).to.be.a('array');
+  });
+
+  it('should return an array of available rooms for today\'s date', () => {
+    // expect(manager.getAvailableRoomsToday().length).to.equal(12);
+  });
+
   // it('should calculate the total revenue for today\'s date', () => {
   //   expect(manager.getTotalRevenueToday()).to.be.a('number');
   // });
-  //
+
   // it('should calculate the percentage of rooms occupied for today\'s date', () => {
-  //   expect(manager.getPercentageOfRoomsOccupiedToday()).to.be.a('number');
+  // expect(manager.getPercentageOfRoomsOccupiedToday()).to.be.a('number');
   // });
-
-
 });
