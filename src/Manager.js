cfae59050cffaa774b2df0e7cb2985d053ffd15b
Madison Randle <madisonrandle25@gmail.com>
2020-04-16 01:38:45 -0600

    Update getAvailableRoomsToday method to iterate through paramaters vs class properties that were removed

--- src/Manager.js
+++ src/Manager.js
@@ -1,34 +1,36 @@
 import $ from 'jquery';
 import Customer from './Customer';
+import moment from 'moment';
 
 class Manager extends Customer {
   constructor(user) {
     super(user, true)
+      this.todaysDate = '2020/02/04';
+      // this.todaysDate = moment().format('YYYY-MM-DD');
       this.availableRooms = [];
       this.occupiedRooms = [];
       this.roomsBookedToday = [];
   };
 
-  // getAvailableRoomsToday() {
-  //
-  //   this.rooms.forEach(room => {
-  //     this.bookings.forEach(booking => {
-  //       if (booking.roomNumber === room.number && booking.date === this.todaysDate) {
-  //         this.roomsBookedToday.push(room);
-  //       };
-  //     });
-  //   });
-  //
-  //   this.rooms.forEach(room => {
-  //     this.bookings.forEach(booking => {
-  //       if (!this.roomsBookedToday.includes(room) && !this.availableRooms.includes(room)) {
-  //         this.availableRooms.push(room);
-  //       }
-  //     })
-  //   });
-  //   return this.availableRooms;
-  // };
-  //
+  getAvailableRoomsToday(rooms, bookings) {
+    rooms.forEach(room => {
+      bookings.forEach(booking => {
+        if (booking.roomNumber === room.number && booking.date === this.todaysDate) {
+          this.roomsBookedToday.push(room);
+        };
+      });
+    });
+
+    rooms.forEach(room => {
+      bookings.forEach(booking => {
+        if (!this.roomsBookedToday.includes(room) && !this.availableRooms.includes(room)) {
+          this.availableRooms.push(room);
+        }
+      })
+    });
+    return this.availableRooms;
+  };
+
   // getTotalRevenueToday() {
   //   return this.rooms.reduce((totalRevenue, room) => {
   //     this.bookings.forEach(booking => {
