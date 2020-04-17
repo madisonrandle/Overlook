4167848b43f3410665cfc09f0e123f4f15bca986
Madison Randle <madisonrandle25@gmail.com>
2020-04-16 19:47:53 -0600

    Update dates to have 4 past, present and future dates based on today's date

--- src/test-data/bookings-test-data.js
+++ src/test-data/bookings-test-data.js
@@ -3,85 +3,85 @@ const bookingsTestData = {
     {
       "id": "5fwrgu4i7k55hl6sz",
       "userID": 1,
-      "date": "2020/02/04",
+      "date": "2020/04/16",
       "roomNumber": 1,
       "roomServiceCharges": []
     },
     {
       "id": "5fwrgu4i7k55hl6t5",
       "userID": 43,
-      "date": "2020/01/24",
-      "roomNumber": 24,
+      "date": "2020/04/16",
+      "roomNumber": 2,
       "roomServiceCharges": []
     },
     {
       "id": "5fwrgu4i7k55hl6t6",
       "userID": 13,
-      "date": "2020/01/10",
-      "roomNumber": 12,
+      "date": "2019/01/10",
+      "roomNumber": 3,
       "roomServiceCharges": []
     },
     {
       "id": "5fwrgu4i7k55hl6t7",
       "userID": 20,
-      "date": "2020/02/16",
-      "roomNumber": 7,
+      "date": "2020/10/16",
+      "roomNumber": 4,
       "roomServiceCharges": []
     },
     {
       "id": "5fwrgu4i7k55hl6t8",
       "userID": 1,
       "date": "2020/02/05",
-      "roomNumber": 12,
+      "roomNumber": 5,
       "roomServiceCharges": []
     },
     {
       "id": "5fwrgu4i7k55hl6t9",
       "userID": 38,
-      "date": "2020/02/14",
-      "roomNumber": 14,
+      "date": "2020/08/14",
+      "roomNumber": 6,
       "roomServiceCharges": []
     },
     {
       "id": "5fwrgu4i7k55hl6ta",
       "userID": 25,
-      "date": "2020/01/11",
-      "roomNumber": 9,
+      "date": "2019/01/11",
+      "roomNumber": 7,
       "roomServiceCharges": []
     },
     {
       "id": "5fwrgu4i7k55hl6tb",
       "userID": 49,
-      "date": "2020/02/06",
-      "roomNumber": 5,
+      "date": "2020/07/06",
+      "roomNumber": 8,
       "roomServiceCharges": []
     },
     {
       "id": "5fwrgu4i7k55hl6tc",
       "userID": 9,
-      "date": "2020/01/09",
-      "roomNumber": 3,
+      "date": "2019/01/09",
+      "roomNumber": 9,
       "roomServiceCharges": []
     },
     {
       "id": "5fwrgu4i7k55hl6td",
       "userID": 9,
       "date": "2020/05/31",
-      "roomNumber": 20,
+      "roomNumber": 10,
       "roomServiceCharges": []
     },
     {
       "id": "5fwrgu4i7k55hl6te",
       "userID": 9,
-      "date": "2020/01/19",
-      "roomNumber": 8,
+      "date": "2020/04/16",
+      "roomNumber": 11,
       "roomServiceCharges": []
     },
     {
       "id": "5fwrgu4i7k55hl6tf",
       "userID": 9,
-      "date": "2019/01/25",
-      "roomNumber": 2,
+      "date": "2020/04/16",
+      "roomNumber": 12,
       "roomServiceCharges": []
     },
   ]
