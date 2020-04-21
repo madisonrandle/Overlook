864420181ee8f6c0f11b6c744b59674aad22fbb5
Madison Randle <madisonrandle25@gmail.com>
2020-04-20 23:28:12 -0600

    Add event listener to invoke Customer Post method

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -35,31 +35,25 @@ const domUpdates = {
     });
 
     const availableRoomsToday = user.getAvailableRoomsToday(hotelObj.rooms, hotelObj.bookings, user.todaysDate).length;
-
     const occupiedRoomsToday = user.getPercentageOfRoomsOccupiedToday(hotelObj.rooms);
-
     const totalRevenueToday = user.getTotalRevenueToday(hotelObj.rooms, hotelObj.bookings);
 
     $('body').html(`
       <section id="user-access-page">
         <header id="header"></header>
         <main id="main">
-
           <section id="occupied-rooms">
             <h2>Occupied</h2>
             <p id="percent-occupied-rooms">${occupiedRoomsToday}%</p>
           </section>
-
           <section id="available-rooms">
             <h2>Available</h2>
             <p id="num-available-rooms">${availableRoomsToday} rooms</p>
           </section>
-
           <section id="revenue-today">
             <h2>Total Revenue</h2>
             <p id="num-revenue-today">${totalRevenueToday}</p>
           </section>
-
         </main>
       </section>
     `);
@@ -104,10 +98,16 @@ const domUpdates = {
         Object.keys(presentBooking).forEach(date => {
           let formattedDate = moment(`${date}`, 'YYYY/MM/DD').format('l');
           $('#today-bookings').append(`
-            <p>${formattedDate}</p>
-            <p>${presentBooking[date].roomType}</p>
-            <p>${presentBooking[date].numBeds} ${presentBooking[date].bedSize}</p>
-            <p>${presentBooking[date].costPerNight.toLocaleString("en-US", {style: "currency", currency: "USD"})}</p>
+            <div style="border: 1px solid black;">
+
+              <p style="padding: 1rem;">${formattedDate}</p>
+
+              <p style="padding: 1rem;">${presentBooking[date].roomType}</p>
+              <p style="padding: 1rem;">${presentBooking[date].numBeds} ${presentBooking[date].bedSize}</p>
+
+              <p style="padding: 1rem;">${presentBooking[date].costPerNight.toLocaleString("en-US", {style: "currency", currency: "USD"})}</p>
+
+            </div>
           `);
         });
       });
@@ -178,7 +178,7 @@ const domUpdates = {
             <p style="padding: 1rem;">${room.roomType}</p>
             <p style="padding: 1rem;">${room.numBeds} ${room.bedSize}</p>
             <p style="padding: 1rem;">${room.costPerNight.toLocaleString("en-US", {style: "currency", currency: "USD"})}</p>
-            <button class="book-room">Book Room</button>
+            <button class="book-room" id=${room.number}>Book Room</button>
           </div>
         `)
       });
@@ -196,23 +196,18 @@ const domUpdates = {
               <p style="padding: 1rem;">${room.roomType}</p>
               <p style="padding: 1rem;">${room.numBeds} ${room.bedSize}</p>
               <p style="padding: 1rem;">${room.costPerNight.toLocaleString("en-US", {style: "currency", currency: "USD"})}</p>
-              <button class="book-room">Book Room</button>
+              <button class="book-room" id=${room.number}>Book Room</button>
             </div>
           `)
         });
       });
 
+      $(".book-room").click((e) => {
+        user.postBooking(e.target.id);
+      })
     });
-
-
-    $('.book-room').on('click', (e) => {
-      console.log(e.target);
-      console.log('hi');
-      // user.postBooking(hotelObj.bookings);
-    })
-
-
   },
+
 
   invalidLogin: () => {
     console.log('username or password is incorrect');
